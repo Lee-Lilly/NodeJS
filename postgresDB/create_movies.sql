083ea408af342f5f6b8874bac190798c53bdd898
Lee C. Degeilh <p3025@student.jamk.fi>
2020-10-15 13:14:39 +0300

    initialized nodejs using express framework

---
+++ postgresDB/create_movies.sql
@@ -0,0 +1,6 @@
+CREATE TABLE movies (
+  id serial PRIMARY KEY,
+  title VARCHAR (200) NOT NULL,
+  director VARCHAR (200) NOT NULL, 
+  year INTEGER NOT NULL 
+);
---
+++ postgresDB/insert_movies.sql
@@ -0,0 +1,3 @@
+INSERT INTO movies (title, year, director) VALUES ('Star Wars: Episode IX - The Rise of Skywalker', 2019,'J.J. Abrams');
+INSERT INTO movies (title, year, director) VALUES ('The Irishman', 2019, 'Martin Scorsese');
+INSERT INTO movies (title, year, director) VALUES ('Harry Potter and the Sorcerers Stone', 2001, 'Chris Columbus');
