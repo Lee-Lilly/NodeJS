55273b0f4f6dfee7b6a1a5a4802110ebf1c0ffa1
Lee C. Degeilh <p3025@student.jamk.fi>
2020-10-15 13:41:39 +0300

    connect to database

---
+++ nodemoviedb/pgdb/dbconfig.js
@@ -0,0 +1,20 @@
+// import node-postgres Pool module. 
+const { Pool } = require('pg');
+
+// create a new instance of the Pool module and define connection settings 
+// get connection to our Postgres SQL database movie
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "movie",
+    password: "postgres"
+});
+
+// export our module named as 'query'
+// query invokes node-postgres pool.query() function with parameters (text, params) 
+// pool.query() can be used to execute SQL statements to the database
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+}
+
