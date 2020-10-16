44913bbd0bfbc34201f93bec2dacdc0c5d16b5cd
Lee C. Degeilh <p3025@student.jamk.fi>
2020-10-16 15:15:30 +0300

    listMovies,searchMovie, updateMovie, addMovie, deleteMovie

--- nodemoviedb/index.js
+++ nodemoviedb/index.js
@@ -6,14 +6,23 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-// use queries exported from dbqueries
+// use methods exported from dbqueries
 const query = require('./pgdb/dbqueries');
 
 //get all movies
 app.get("/api/movies", query.getAllmovies);
 
 // Get movie by id
-app.get("/api/movies/:id", query.getMoviebyId)
+app.get("/api/movies/:id", query.getMoviebyId);
+
+//add new movie
+app.post("/api/movies", query.addNewMovie);
+
+//delete movie by id
+app.delete("/api/movies/:id", query.deleteMoviebyId);
+
+//update movie by id
+app.put("/api/movies/:id", query.updateMoviebyId);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
--- nodemoviedb/pgdb/dbqueries.js
+++ nodemoviedb/pgdb/dbqueries.js
@@ -2,7 +2,7 @@
 const db = require('./dbconfig');
 
 //get all movies
-const list_movies = (req, res) => {
+const listMovies = (req, res) => {
     // method query exported from dbconfig.js, takes two parameters
     // SQL statement 
     //a error-first callback function be executed immediately
@@ -17,17 +17,17 @@ const list_movies = (req, res) => {
 
 // Get movie by id
 //use parameterized query, to avoid the risk of SQL injection
-const search_movie = (req, res) => {
+const searchMovie = (req, res) => {
     // With node-postgres library, we can define queries as an object.
     // Query object contains text attribute that is a SQL statement 
     // and values attribute that contains query parameter values.
     // '$1' means the first element in the 'valuess' array 
-    const search_by_id = {
+    const search_query = {
         text: 'SELECT * FROM movies WHERE id = $1',
         values: [req.params.id]
     }
 
-    db.query(search_by_id, (err, result) => {
+    db.query(search_query, (err, result) => {
         if (err) {
             return console.error('Error executing query', err.stack)
         }
@@ -40,8 +40,69 @@ const search_movie = (req, res) => {
     })
 }
 
+// add new movie
+const addMovie = (req, res) => {
+    //extra movie body from the request body
+    const newMovie = req.body;
+
+    //query object, values array gets from req.body
+    const insert_query = {
+        text: 'INSERT INTO movies (title, director, year) VALUES ($1, $2, $3)',
+        values: [newMovie.title, newMovie.director, newMovie.year]
+    }
+
+    // insert to database
+    db.query(insert_query, (err, result) => {
+        if (err) {
+            return console.error('Error executing query', err.stack);
+        }
+    })
+    
+    res.json(newMovie);
+}
+
+//delete movie
+const deleteMovie = (req, res) => {
+    // define query object
+    const delete_query = {
+        text: 'DELETE FROM movies WHERE id = $1',
+        values: [req.params.id]
+    }
+
+    // delete from database
+    db.query(delete_query, (err, result) => {
+        if (err)
+            return console.error('Error executing query', err.stack);
+    })
+
+    res.sendStatus(204);
+}
+
+//update movie
+const updateMovie = (req, res) => {
+    //extract edited movie from the request body
+    const editedMovie = req.body;
+    
+    // define query object
+    const update_query = {
+        text: 'UPDATE movies SET title = $1, director = $2, year = $3 WHERE id = $4',
+        values: [editedMovie.title, editedMovie.director, editedMovie.year, req.params.id]
+    }
+
+    //update to database
+    db.query(update_query, (err, result) => {
+        if(err)
+            return console.error('Error executing query', err.stack);
+    })
+
+    res.json(editedMovie);
+}
+
 //export functions
 module.exports = {
-    getAllmovies: list_movies,
-    getMoviebyId: search_movie
+    getAllmovies: listMovies,
+    getMoviebyId: searchMovie,
+    addNewMovie: addMovie,
+    deleteMoviebyId: deleteMovie,
+    updateMoviebyId: updateMovie
 }
